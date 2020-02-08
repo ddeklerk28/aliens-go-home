a6ede8532ad3c27366d1fb7832a6775a14aba012
Dirk de Klerk <dirk.de.klerk@advance.io>
2020-02-08 09:07:07 +0200

    Added Cannon and cannon tracking code

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
+import CannonBall from './CannonBall';
 
-const Canvas = () => {
+const Canvas = (props) => {
     const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
 
     console.log(viewBox);
@@ -12,14 +14,21 @@ const Canvas = () => {
         <svg
             id="aliens-go-home-canvas"
             preserveAspectRatio="xMaxYMax none"
+            onMouseMove={props.trackMouse}
             viewBox={viewBox}
         >
             <Sky />
             <Ground />
-            <CannonPipe rotation={45}/>
+            <CannonPipe rotation={props.angle}/>
             <CannonBase />
+            <CannonBall position={{x: 0, y: -100}}/>
         </svg>
     );
+};
+
+Canvas.propTypes = {
+    angle: PropTypes.number.isRequired,
+    trackMouse: PropTypes.func.isRequired,
 };
 
 export default Canvas;
