886d70fa7610868e0de2dd265a6e218db90641d9
Dirk de Klerk <dirk.de.klerk@advance.io>
2020-02-08 03:41:39 +0200

    Added some background components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Canvas from './components/Canvas';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <h1>We will create an awesome game with React, Redux and SVG!</h1>
-    </div>
+      <Canvas />
   );
 }
 
